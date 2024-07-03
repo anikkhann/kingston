31dd3b2dac858fe5394d4185ee03bfb6598799eb
anikkhann46 <anikkhann46@gmail.com>
2024-07-03 22:11:42 +0600

    new changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />}  exact/>
         <Route path="/contact" element={<Contact />}  exact/>
-        {/* <Route path="*" element={<NotFound />}  exact/> */}
+        <Route path="*" element={<NotFound />}  exact/>
       </Routes>
     </>
   );
